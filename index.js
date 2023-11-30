452e8ce1e95cef8eb66e12c012c226b5e3626c49
MelLayanne <mel.layn2006@gmail.com>
2023-11-30 06:49:44 -0300

    exibição das tarefas na página home concluída

--- index.js
+++ index.js
@@ -50,10 +50,8 @@ app.get('/', (requisicao, resposta) => {
                 completa: dado.completa === 0 ? false : true
             }
         })
-
+        resposta.render('home', {tarefas})
     })
-    
-    resposta.render('home')
 }) 
 
 const conexao = mysql.createConnection({
